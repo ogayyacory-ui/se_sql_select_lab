cedfb2c30275dedc5313e10b11a50f9d42f5c377
cory <cory.martel@student.moringaschool.com>
2026-07-09 10:25:27 +0300

    modified the main file

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pandas as pd
 # Connect to the database
 conn = sqlite3.connect('data.sqlite')
 
-
 # STEP 2
 # Replace None with your code
 df_first_five = pd.read_sql_query("SELECT employeeNumber, lastName FROM employees", conn)
@@ -18,24 +17,24 @@ df_five_reverse = pd.read_sql_query("SELECT lastName, employeeNumber FROM employ
 
 # STEP 4
 # Replace None with your code
-df_alias = pd.read_sql_query("SELECT name AS employee_name FROM employees", conn)
+df_alias = pd.read_sql_query("SELECT lastName, employeeNumber AS ID FROM employees", conn)
 
 # STEP 5
 # Replace None with your code
-df_executive = pd.read_sql_query("SELECT * FROM employees WHERE position = 'Executive'", conn)  
+df_executive = pd.read_sql_query("SELECT  *, CASE WHEN jobTitle = 'Executive' THEN 'Yes' ELSE 'No' END AS is_executive FROM employees", conn)  
 
 # STEP 6
 # Replace None with your code
-df_name_length = pd.read_sql_query("SELECT name, LENGTH(name) AS name_length FROM employees", conn)
+df_name_length = pd.read_sql_query("SELECT lastName, LENGTH(lastName) AS name_length FROM employees", conn)
 
 # STEP 7
 # Replace None with your code
-df_short_title = pd.read_sql_query("SELECT * FROM employees WHERE LENGTH(title) < 10", conn)
+df_short_title = pd.read_sql_query("SELECT * FROM employees WHERE LENGTH(jobTitle) < 10", conn)
 
 # STEP 8
 # Replace None with your code
-sum_total_price = pd.read_sql_query("SELECT SUM(price) AS total_price FROM products", conn).iloc[0, 0]
+sum_total_price = pd.read_sql_query("SELECT SUM(buyPrice) AS total_price FROM products", conn).iloc[0, 0]
 
 # STEP 9
 # Replace None with your code
-df_day_month_year = pd.read_sql_query("SELECT strftime('%d', hire_date) AS day, strftime('%m', hire_date) AS month, strftime('%Y', hire_date) AS year FROM employees", conn)
+df_day_month_year = pd.read_sql_query("SELECT strftime('%d', hireDate) AS day, strftime('%m', hireDate) AS month, strftime('%Y', hireDate) AS year FROM employees", conn)
